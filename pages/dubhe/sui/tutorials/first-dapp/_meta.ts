94b2e1c94c712e339dcd80ba47f908df6b991ac5
henryliu <17600047539@163.com>
2024-12-03 17:18:58 +0800

    update

---
+++ pages/dubhe/sui/tutorials/first-dapp/_meta.ts
@@ -0,0 +1,7 @@
+export default {
+  "create-a-new-dapp":"Create A New Dapp",
+  "writing-system-logic-and-publish":"Writing System Logic and Publish",
+  "client-dapp-with-dubhe-sdk":"Client DApp with Dubhe SDK",
+  "update-and-migrate":"Update And Migrate ",
+  "debugging-and-testing-package":"Debugging-and-Testing-Package",
+}
